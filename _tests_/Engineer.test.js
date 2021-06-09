9d1ec07e2773819f81d59eaa8ccf2cc5021a6e28
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-06-09 16:25:18 -0400

    Added code in Engineer.test.js

---
+++ _tests_/Engineer.test.js
@@ -0,0 +1,21 @@
+// ENGINEER JEST TEST
+// ---------------------------------------------------------------------------
+const Engineer = require("../lib/Engineer");
+
+test("Set GitHub Account via constructor", () => {
+    const testValue = "GitHubUser";
+    const emp = new Engineer("Cole", 1, "email@email.com", testValue);
+    expect(emp.github).toBe(testValue);
+});
+
+test("getRole() function should return 'Enginner'", () => {
+    const testValue = "Engineer";
+    const emp = new Engineer("Cole", 1, "email@email.com", "GitHubUser");
+    expect(emp.getRole()).toBe(testValue);
+});
+
+test("getGitHub() function should return Github Username", () => {
+    const testValue = "GitHubUser";
+    const emp = new Engineer("Cole", 1, "email@email.com", testValue);
+    expect(emp.getGitHub()).toBe(testValue);
+});
